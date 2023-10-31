d895f87eb148d7a3d7d2b405ff8a7fac7d124f99
Mykhailo Denysov <mykhailo.denysov@gmail.com>
2023-10-31 05:51:58 +0100

    changed paths in generate_yaml.py

--- backend/generate_yaml.py
+++ backend/generate_yaml.py
@@ -4,8 +4,8 @@ import os
 template = Template("app.jinja")
 template_loader = FileSystemLoader(searchpath="./")
 template_env = Environment(loader=template_loader)
-template = template_env.get_template("app-spec.jinja")
+template = template_env.get_template("backend/app-spec.jinja")
 r = template.render(env=os.environ)
 
-with open("app-spec.yaml", "w") as file:
+with open("backend/app-spec.yaml", "w") as file:
     file.write(r)
