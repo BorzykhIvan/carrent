b70a07b18948fef4667266e91f22ac52d524f096
BorzykhIvan <venomgtspeed@gmail.com>
2023-11-18 21:49:43 +0100

    calendar

--- frontend/reservation.js
+++ frontend/reservation.js
@@ -2,30 +2,29 @@ document.addEventListener("DOMContentLoaded", function () {
   const carList = document.getElementById('carList');
 
   carList.addEventListener('click', function (event) {
-      const target = event.target;
+    const target = event.target;
 
-      // Проверяем, была ли нажата кнопка "carbutton" или текст "ZAREZERWUJ"
-      if (target.classList.contains('carbutton') || (target.classList.contains('deleteCar') && target.textContent === 'ZAREZERWUJ')) {
-          const carElement = target.closest('.car');
-          const carId = target.getAttribute('data-car-id');
-          const imageUrl = carElement.querySelector('.carimgg').getAttribute('src');
-          const brand = carElement.querySelector('.brand').textContent;
-          const model = carElement.querySelector('.model').textContent;
+    // Проверяем, была ли нажата кнопка "carbutton" или текст "ZAREZERWUJ"
+    if (target.classList.contains('carbutton') || (target.classList.contains('deleteCar') && target.textContent === 'ZAREZERWUJ')) {
+      const carElement = target.closest('.car');
+      const carId = target.getAttribute('data-car-id');
+      const imageUrl = carElement.querySelector('.carimgg').getAttribute('src');
+      const brand = carElement.querySelector('.brand').textContent;
+      const model = carElement.querySelector('.model').textContent;
 
-          // Проверяем, существует ли уже блок с классом "reservation"
-          if (!document.querySelector('.reservation')) {
-              showReservationInfo(imageUrl, brand, model);
-          }
+      // Проверяем, существует ли уже блок с классом "reservation"
+      if (!document.querySelector('.reservation')) {
+        showReservationInfo(imageUrl, brand, model);
       }
+    }
   });
 
+  function showReservationInfo(imageUrl, brand, model) {
+    // Создаем блок для отображения информации о машине
+    const reservationDiv = document.createElement('div');
+    reservationDiv.classList.add('reservation');
 
-    function showReservationInfo(imageUrl, brand, model) {
-        // Создаем блок для отображения информации о машине
-        const reservationDiv = document.createElement('div');
-        reservationDiv.classList.add('reservation');
-
-        reservationDiv.innerHTML = `
+    reservationDiv.innerHTML = `
         <div class="reservation_container">
           <div class="carimg">
             <img class="carimgg_reservation" src="${imageUrl}" alt="Car Image">
@@ -51,29 +50,69 @@ document.addEventListener("DOMContentLoaded", function () {
             <div class="kod_reservation"><p>Kod promocyjny</p></div>
             <div class="kod_reservation1"><p>Wpisz...</p></div>
           </div>
-          
+
           <div class="price_reservation">
             <div class="price_reservation_text">Koszt wynajmu wynosi:</div>
             <div class="price_reservation1">
-            <div class="price_r">539 ZŁ</div>
-            <div class="price_button">Oblicz</div>
+              <div class="price_r">539 ZŁ</div>
+              <div class="price_button">Oblicz</div>
             </div>
           </div>
           <div class="carbutton_reservation">
-          <p >ZAREZERWUJ</p>  
-        </div>
-        <button class="close-button"><img class="close_button" src="https://fra1.digitaloceanspaces.com/carrentbucket/static/icon%20_cancel.svg"></button>
+            <p >ZAREZERWUJ</p>  
+          </div>
+          <button class="close-button"><img class="close_button"src="https://fra1.digitaloceanspaces.com/carrentbucket/static/icon%20_cancel.svg"></button>
         </div>
       `;
 
-        // Добавляем блок в body
-        document.body.appendChild(reservationDiv);
+      document.body.appendChild(reservationDiv);
 
-        // Добавляем обработчик события для кнопки закрытия
-        const closeButton = reservationDiv.querySelector('.close-button');
-        closeButton.addEventListener('click', () => {
-            // Удаляем блок при нажатии на кнопку закрытия
-            document.body.removeChild(reservationDiv);
-        });
+      // Находим элемент с классом "flatpickr-calendar"
+      const startDatePicker = reservationDiv.querySelector('.start_date_reservation1');
+      
+      // Инициализируем flatpickr
+      const fp = flatpickr(startDatePicker, {
+        enableTime: false,
+        dateFormat: "Y-m-d",
+        locale: {
+          firstDayOfWeek: 1,
+          months: {
+            shorthand: ["Sty", "Lut", "Mar", "Kwi", "Maj", "Cze", "Lip", "Sie", "Wrz", "Paź", "Lis", "Gru"],
+            longhand: [
+              "Styczeń",
+              "Luty",
+              "Marzec",
+              "Kwiecień",
+              "Maj",
+              "Czerwiec",
+              "Lipiec",
+              "Sierpień",
+              "Wrzesień",
+              "Październik",
+              "Listopad",
+              "Grudzień"
+            ],
+          },
+          weekdays: {
+            shorthand: ["Nie", "Pon", "Wto", "Śro", "Czw", "Pią", "Sob"],
+            longhand: [
+              "Niedziela",
+              "Poniedziałek",
+              "Wtorek",
+              "Środa",
+              "Czwartek",
+              "Piątek",
+              "Sobota"
+            ],
+          },
+        },
+      });
+  
+      // Добавляем обработчик события для кнопки закрытия
+      const closeButton = reservationDiv.querySelector('.close-button');
+      closeButton.addEventListener('click', () => {
+        // Удаляем блок при нажатии на кнопку закрытия
+        document.body.removeChild(reservationDiv);
+      });
     }
-});
+  });
