456ec6293e56ff7f11645018b1ea0b69641f2d4f
Mykhailo Denysov <mykhailo.denysov@gmail.com>
2023-11-29 23:35:20 +0100

    swagger documentation

--- backend/backend/views/referrals.py
+++ backend/backend/views/referrals.py
@@ -1,18 +1,40 @@
+from rest_framework import status, serializers
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import status
-from django.conf import settings
+
+from drf_spectacular.utils import extend_schema, OpenApiResponse, inline_serializer
+
 from ..serializers import RefferalTokenSerializer
 
 
 class RefferalView(APIView):
     permission_classes = [IsAuthenticated]
 
+    @extend_schema(
+        description="Get referral token",
+        responses={200: OpenApiResponse(response=RefferalTokenSerializer)},
+    )
     def get(self, request):
         token = request.user.referral_token
         return Response({"token": token})
 
+    @extend_schema(
+        description="Activate referral token",
+        request=RefferalTokenSerializer,
+        responses={
+            200: OpenApiResponse(
+                description="Referral token successfully activated!",
+                response=RefferalTokenSerializer,
+            ),
+            400: OpenApiResponse(
+                description="Referral token is invalid",
+                response=inline_serializer(
+                    "ErrorSerializer", fields={"error": serializers.CharField()}
+                ),
+            ),
+        },
+    )
     def post(self, request):
         serializer = RefferalTokenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
