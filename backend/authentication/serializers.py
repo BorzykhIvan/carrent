44ca9a2ebe9bf899b550dfdceb1a1f9895c9b483
Mykhailo Denysov <mykhailo.denysov@gmail.com>
2023-12-15 21:02:21 +0100

    fixed bug with user update

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -45,11 +45,20 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
     def update(self, instance, validated_data):
-        address_dict = validated_data.pop("address")
-        instance.address.city = address_dict.pop("city")
-        instance.address.building = address_dict.pop("building")
-        instance.address.zip_code = address_dict.pop("zip_code")
-        instance.address.street = address_dict.pop("street")
+        if "address" in validated_data:
+            address_dict = validated_data.pop("address")
+            instance.address.city = address_dict.get("city", instance.address.city)
+            instance.address.building = address_dict.get(
+                "building", instance.address.building
+            )
+            instance.address.zip_code = address_dict.get(
+                "zip_code", instance.address.zip_code
+            )
+            instance.address.street = address_dict.get(
+                "street", instance.address.street
+            )
         for field, data in validated_data.items():
             setattr(instance, field, data)
         return instance
+
+    # def send_change_request(self):
