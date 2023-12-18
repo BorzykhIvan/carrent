7cad714a3df31d7e28af35d0c4e8858e935e1b53
Mykhailo Denysov <mykhailo.denysov@gmail.com>
2023-12-18 03:49:18 +0100

    chat history

--- frontend/pomoc.js
+++ frontend/pomoc.js
@@ -3,17 +3,35 @@ const msgerInput = get(".msger-input");
 const msgerChat = get(".msger-chat");
 
 var authToken = getCookie('authToken');
-const BOT_MSGS = [
-  "Zaczekaj na odpowiedz jednego z naszych operatorow",
-];
 
 
 const BOT_IMG = "https://fra1.digitaloceanspaces.com/carrentbucket/static/bot.svg";
 const PERSON_IMG = "https://fra1.digitaloceanspaces.com/carrentbucket/static/user.svg";
 const BOT_NAME = "BOT";
-const PERSON_NAME = "User";
+let PERSON_NAME;
+getUser(authToken).then(userData => { PERSON_NAME = `${userData.first_name} ${userData.last_name}` })
 
-msgerForm.addEventListener("submit", event => {
+document.addEventListener("DOMContentLoaded", function () {
+  fetch('https://carrent-w2et2.ondigitalocean.app/api/chat/', {
+    method: 'GET',
+    headers: {
+      'accept': 'application/json',
+      'Authorization': `Token ${authToken}`,
+    }
+  }).then(response => response.json())
+    .then(response => {
+      // console.log(response);
+      response.forEach(message => {
+        if (message.is_admin == null) {
+          appendMessage(PERSON_NAME, PERSON_IMG, "right", message.content, message.date)
+        } else {
+          appendMessage(BOT_NAME, BOT_IMG, "left", message.content, message.date)
+        }
+      });
+    })
+})
+
+msgerForm.addEventListener("submit", event => { // отправляем хуету (сообщение)
   event.preventDefault();
 
   const msgText = msgerInput.value;
@@ -23,26 +41,30 @@ msgerForm.addEventListener("submit", event => {
   msgerInput.value = "";
 
   fetch('https://carrent-w2et2.ondigitalocean.app/api/chat/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Token ${authToken}`,
-        },
-        body: JSON.stringify({
-            'content': msgText
-        }),
-    }).then(response => {
-        if (!response.ok) {
-            throw new Error('Ошибка при получении данных ');
-        }
-        return response.json();
-    }).then(ordersData => {
-        console.log(ordersData);
-    })
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Token ${authToken}`,
+    },
+    body: JSON.stringify({
+      'content': msgText
+    }),
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Ошибка при получении данных ');
+    }
+    return response.json();
+  })
 });
 
-function appendMessage(name, img, side, text) {
+function appendMessage(name, img, side, text, date = null) {
   //   Simple solution for small apps
+  if (!date) {
+    date = formatDate(new Date())
+  } else {
+    [yyyy, mm, dd, hh, mi] = date.split(/[/:\-T]/)
+    date = `${hh}:${mi}`
+  }
   const msgHTML = `
     <div class="msg ${side}-msg">
       <div class="msg-img" style="background-image: url(${img})"></div>
@@ -50,7 +72,7 @@ function appendMessage(name, img, side, text) {
       <div class="msg-bubble">
         <div class="msg-info">
           <div class="msg-info-name">${name}</div>
-          <div class="msg-info-time">${formatDate(new Date())}</div>
+          <div class="msg-info-time">${date}</div>
         </div>
 
         <div class="msg-text">${text}</div>
@@ -62,15 +84,7 @@ function appendMessage(name, img, side, text) {
   msgerChat.scrollTop += 500;
 }
 
-function botResponse() {
-  const r = random(0, BOT_MSGS.length - 1);
-  const msgText = BOT_MSGS[r];
-  const delay = msgText.split(" ").length * 100;
 
-  setTimeout(() => {
-    appendMessage(BOT_NAME, BOT_IMG, "left", msgText);
-  }, delay);
-}
 
 // Utils
 function get(selector, root = document) {
@@ -84,6 +98,3 @@ function formatDate(date) {
   return `${h.slice(-2)}:${m.slice(-2)}`;
 }
 
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
